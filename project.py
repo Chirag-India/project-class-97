fa6ce4933a2af43df835c2188229cd8bdc7736f5
Chirag-India <61911921+Chirag-India@users.noreply.github.com>
2021-01-22 20:17:12 +0530

    Add files via upload

---
+++ project.py
@@ -0,0 +1,9 @@
+months = int(input("ENTER THE MONTHE U ARE CODING FROM : "))
+print(months) 
+
+if (months>10):
+    print("u are a pro")
+elif(months>5):
+    print("keep coding forever")
+else:
+    print("all the best for your future")        
